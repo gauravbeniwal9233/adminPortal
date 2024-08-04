f5e37c915a4877a3ed2a1d2efb8e9e96f730d721
iq-GauravBeniwal <gaurav.beniwal@indoqubix.com>
2024-08-04 12:54:56 -0700

    don't do environement links like this for dev and prod

---
+++ src/app/shared/utility/global.ts
@@ -0,0 +1,12 @@
+// export class Global {
+//     // For Production
+//     public static BASE_API_PATH = "http://sahosoftweb.com/api/";
+//     public static BASE_IMAGE_PATH = "http://sahosoftweb.com/images/";
+//     public static BASE_USERS_IMAGES_PATH = "http://sahosoftweb.com/users/";
+
+
+    // For Development
+    // public static BASE_API_PATH = "http://sahosoftweb.com/test/api/";
+    // public static BASE_IMAGE_PATH = "http://sahosoftweb.com/test/images/";
+    // public static BASE_USERS_IMAGES_PATH = "http://sahosoftweb.com/test/users/";
+// }
