5fcbf981098ddca1f097dcc2698b9f8c7bffdd68
iq-GauravBeniwal <gaurav.beniwal@indoqubix.com>
2024-08-04 12:54:24 -0700

    environment dynamically

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    // For Production
+    BASE_API_PATH : "http://sahosoftweb.com/api/",
+    BASE_IMAGE_PATH : "http://sahosoftweb.com/images/",
+    BASE_USERS_IMAGES_PATH : "http://sahosoftweb.com/users/"
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    // For Production
+    BASE_API_PATH : "http://sahosoftweb.com/test/api/",
+    BASE_IMAGE_PATH : "http://sahosoftweb.com/test/images/",
+    BASE_USERS_IMAGES_PATH : "http://sahosoftweb.com/test/users/"
+};
