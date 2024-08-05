92d17c996bb09452d847ac3984ea3a14469f0731
iq-GauravBeniwal <gaurav.beniwal@indoqubix.com>
2024-08-05 03:32:57 -0700

    routing

--- src/app/components/products/products-routing.module.ts
+++ src/app/components/products/products-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './manage/product-list/product-list.component';
+import { AddProductComponent } from './manage/add-product/add-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: 'manage/product-list', component: ProductListComponent },
+      { path: 'manage/add-product', component: AddProductComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ProductsRoutingModule { }
+export class ProductsRoutingModule {}
--- src/app/components/users/users-routing.module.ts
+++ src/app/components/users/users-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListUserComponent } from './list-user/list-user.component';
+import { AddUserComponent } from './add-user/add-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '', children : [
+    {path : 'list-user', component : ListUserComponent},
+    {path : 'add-user', component : AddUserComponent},
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
