cdf04edab7a2a41ec207dac145303896593b2663
iq-GauravBeniwal <gaurav.beniwal@indoqubix.com>
2024-08-05 12:42:55 -0700

    layout route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import path from 'path';
+import { LayoutComponent } from './shared/components/layout/layout.component';
+import { contentRoutes } from './shared/routes/content.routes';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : '', component : LayoutComponent, children : contentRoutes}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
