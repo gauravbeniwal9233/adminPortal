477f72eeb22f2fb941c9965a00163aae8e9beb84
iq-GauravBeniwal <gaurav.beniwal@indoqubix.com>
2024-08-06 12:32:50 -0700

    add authguard can activate so that user is logged out on page refresh

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import path from 'path';
 import { LayoutComponent } from './shared/components/layout/layout.component';
 import { contentRoutes } from './shared/routes/content.routes';
+import { AuthGuard } from './components/auth/auth.guard';
 
 const routes: Routes = [
-  { path : '', component : LayoutComponent, children : contentRoutes}
+  { path : '', component : LayoutComponent, children : contentRoutes, canActivate : [AuthGuard]}
 ];
 
 @NgModule({
