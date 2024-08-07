6cd8abf3dc1a2348ab114145caf0102721f8172d
iq-GauravBeniwal <gaurav.beniwal@indoqubix.com>
2024-08-06 21:55:45 -0700

    add and list products

--- src/app/components/products/products.module.ts
+++ src/app/components/products/products.module.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { ProductsRoutingModule } from './products-routing.module';
 import { AddProductComponent } from './manage/add-product/add-product.component';
 import { ProductListComponent } from './manage/product-list/product-list.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 
 @NgModule({
@@ -13,7 +15,9 @@ import { ProductListComponent } from './manage/product-list/product-list.compone
   ],
   imports: [
     CommonModule,
-    ProductsRoutingModule
+    ProductsRoutingModule,
+    ReactiveFormsModule,
+    NgxDatatableModule,
   ]
 })
 export class ProductsModule { }
